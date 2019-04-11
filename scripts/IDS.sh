96eda1f1eac552f81f9de3a640906810353738ad
taylorradke <taylorradke@gmai.com>
2019-04-11 20:52:43 +1000

    added comments for get_help function and get_args function.

--- scripts/IDS.sh
+++ scripts/IDS.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-
-#Get file from args
-flags={}
-
-function get_help () {
+#Help function to show how the program should be called
+#What options are available for the program and how they should be provided
+function get_help(){
     echo "Usage: bash IDS.sh file [options]"$'\n'
     echo "options:"$'\n'
     echo " -c name    Create a verification file called ‘name’ also display a message 'File created'"
@@ -11,20 +8,32 @@ function get_help () {
     exit
 }
 
-if [ $# -eq 0 ]
-then
-    get_help
-else
-    for i in $@
-    do
-        flag_index=0
-        if [ $i = "-c" ] || [ $i = "-o" ]
-        then
-            flags[$flag_index]=$i
-            flag_index=$(($flag_index + 1))
-        elif [ $i = "--help" ]
-        then
+#Gets the commandline argument flags if provided and stored them in flags_set
+#and also stores their associated value in flag_vals.
+flags_set={}
+flag_vals={}
+
+function get_args(){
+    flags=0
+    for (( i=1; i<=$#; i++)); do
+        if [ ${!i} = "-o" ] || [ ${!i} = "-c" ]; then
+            flags_set[$flags]=${!i}
+            j=$((i+1))
+            flas_vals[$flags]=${!j}
+            flags=$((flags+1))
+        elif [ ${!i} = "--help" ]; then
             get_help
         fi
     done
+}
+#Check if no arguments are provided
+if [ $# -eq 0 ]; then
+    get_help
+else
+    get_args $@
 fi
+
+#Prints out command and associated value for testing purposes
+for ((i = 0; i < ${#flags_set}; i++)); do
+    echo "${flags_set[$i]} ${flags_vals[$i]}"
+done
