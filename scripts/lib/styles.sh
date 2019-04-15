99bcc4e86f675f351141fb09bc0454486c2bf026
Taylor Radke <taylor.radke@outlook.com>
2019-04-15 22:42:51 +1000

    Seperated styles for use in console output into its own file

---
+++ scripts/lib/styles.sh
@@ -0,0 +1,8 @@
+#Styles for use in output messages
+bold=$(tput bold)
+normal=$(tput sgr0)
+italics=$(tput sitm)
+underline=$(tput smul)
+reset_underline=$(tput rmul)
+standout=$(tput smso)
+end_standout=$(tput rmso)
