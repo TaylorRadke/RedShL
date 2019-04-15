f0c6e327193a59a3029af25e42ca7ea36353cfcf
Taylor Radke <taylor.radke@outlook.com>
2019-04-15 22:48:03 +1000

    Renamed scripts/ to src/

---
+++ src/IDS.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Import functions from lib/
+source lib/flags.sh
+source lib/styles.sh
+
+#Get help if no arguments are provided
+[ $# -eq 0 ] && { get_help; }
+
+#Get help if help arg provided
+help_arg_provided $@
+
+#get arg
+parse_args $@
