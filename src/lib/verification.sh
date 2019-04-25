a21e349f9fd0bf80477a9d17e50b99fa761d3833
TaylorRadke <28990697+TaylorRadke@users.noreply.github.com>
2019-04-26 08:13:41 +1000

    Added comments to verification.sh file

--- src/lib/verification.sh
+++ src/lib/verification.sh
@@ -1,4 +1,9 @@
+# TODO: add functions for writing file owner, creator, permissions, last modified, when it was created
+#       Hash digest if a file, something about symlinks
+#       Create seperate file from the verification file created when -c is used to save states for checking changes 
 
+
+# Create a verification file if the -c flag is provided.
 function create_verification_file {
     if [[ $c_FLAG != "" ]]; then
         verification_file="$(pwd)/tracking/${c_FLAG}.txt"
@@ -12,16 +17,23 @@ function create_verification_file {
 fi
 }
 
+#Prints the given symbol(arg 2) the number of times given by arg 1.
 function print_symbols {
     for ((i=0; i < $1; i++)); do
         printf "$2" >> $verification_file
     done
 }
 
+# Write the table header for the verification file from the input
  function write_file_headers {
+   
+    # The numbers of characters in the current line being processed from the input file 
     line_length=${#line}
+
+    # Width of the table, long enough that large file paths should not break the table format
     header_length=100
 
+    # Define the start of a new table with input file name in table header
     print_symbols $header_length "="
     printf "\n| $line" >> $verification_file
 
@@ -31,23 +43,31 @@ function print_symbols {
     print_symbols $header_length "="
 }
 
+
+# Write a row to the table which shows the file type of the current line, either file or directory
 function write_file_type {
     file=$1
     
     printf "\n| File Type: " >> $verification_file
+
+    # If function arg 1 is file then set file to “file”
     if [ -f $1 ]; then
         type="file"
+    # if function arg 1 is directory then set file to “directory”
     elif [ -d $1 ]; then
         type="directory"
     fi
 
     printf "$type" >> $verification_file
     length=${#type}
+
+    # Print spaces from end of file type to end of row leaving a space for table border.
     print_symbols $((header_length - 14 - length)) " "
     printf "|\n" >> $verification_file
     print_symbols $header_length "-"
 }
 
+# Prints the full file path of input line of the given line is a link to a relative file.
 function write_full_file_path {
     full_path=$(readlink -f $1)
     printf "\n| Full Path: " >> $verification_file
@@ -58,8 +78,10 @@ function write_full_file_path {
     print_symbols $header_length "-"
 }
 
+# Save the states of the files from the input file to monitor any changes
 function verify_input_file {
     while IFS= read -r line || [ -n "$line" ] ; do
+        # If the file exists write a table for it
         if [ -e $line ]; then
             write_file_headers
             write_file_type $line
