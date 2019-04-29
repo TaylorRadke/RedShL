91498ace7a8c0c833dc038dd8356fc6aed5bd79f
Taylor Radke <taylor.radke@outlook.com>
2019-04-29 15:21:02 +1000

    Created the test_dir for the use of testing the script

---
+++ test_dir/python_test_dir/README.md
@@ -0,0 +1,1 @@
+#This directory is purely for testing the RedShL script as contents for the test_dir
---
+++ test_dir/python_test_dir/src/main.py
@@ -0,0 +1,2 @@
+#This script is purely for testing the RedShL script as contents for the test_dir
+print("this is a python program")
